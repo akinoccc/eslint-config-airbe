551de8996a561cca395892f73b7b43f15bfa9034
akinocccc <akinochen@foxmail.com>
2024-07-17 22:37:19 +0800

    chore: fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,4 @@
 import defineConfig from './dist/index.mjs'
-console.log(defineConfig()[6].languageOptions)
-export default defineConfig()
 
 export default defineConfig({
   js: true,
@@ -8,29 +6,3 @@ export default defineConfig({
   vue: true,
   ignores: ['dist/*', 'node_modules/*'],
 })
-
-// export default [
-//   eslint.configs.recommended,
-//   ...pluginVue.configs['flat/recommended'],
-//   ...tseslint.config(...tseslint.configs.recommendedTypeChecked, ...tseslint.configs.stylisticTypeChecked),
-//   {
-//     languageOptions: {
-//       parser: vueParser,
-//       parserOptions: {
-//         parser: tseslint.parser,
-//         projectService: true,
-//         EXPERIMENTAL_useProjectService: true,
-//         ecmaFeatures: {
-//           jsx: true
-//         },
-//       },
-//     },
-//     rules: {
-//       // override/add rules settings here
-//       'quotes': ['error', 'single'],
-//       'no-multiple-empty-lines': ['error', { max: 1 }],
-//       '@typescript-eslint/no-explicit-any': 'warn',
-//       '@typescript-eslint/no-unsafe-argument': 'off',
-//     }
-//   }
-// ]
